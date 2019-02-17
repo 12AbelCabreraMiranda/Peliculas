aa8b048b56ae3be3baec34ff2f5a09dbc0b8aba3
12AbelCabreraMiranda <leg.abelr@gmail.com>
2019-02-17 02:02:44 -0600

    pelis destacadas

---
+++ js/imgDestacado.js
@@ -0,0 +1,19 @@
+
+var cont=0
+function cambia() {
+cont = cont % 2;
+
+    if (cont==1){
+        document.getElementById("fotocambia").src="imgDestacado/destacado1.jpg";
+        
+    }else{
+            document.getElementById("fotocambia").src="imgDestacado/destacado2.jpg";
+            
+        }
+        cont++;
+    }
+
+function inicio() {
+setInterval(cambia, 10000);
+}
+window.onload=inicio;
