35dc7021072bc90889f0ad263d59598bb298d3cc
12AbelCabreraMiranda <leg.abelr@gmail.com>
2019-02-18 03:46:10 -0600

    chat facebook modificado tamanio

--- js/modal_movil.js
+++ js/modal_movil.js
@@ -1,6 +1,6 @@
 $('.galeria__img').click(function(e){
     var img = e.target.src;
-    var modal = '<div class="modal" id="modal"><img src="'+img+'" class="modal__img"><div style="cursor:pointer" class="modal__boton" id="modal__boton">X</div></div>';
+    var modal = '<div class="modal " id="modal"><img src="'+img+'" class="modal__img"><div style="cursor:pointer" class="modal__boton" id="modal__boton">X</div></div>';
 
     $('body').append(modal);
     $('#modal__boton').click(function(){
